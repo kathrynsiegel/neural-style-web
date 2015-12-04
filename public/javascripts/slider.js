a3b6f7fbfbc9ec54d9a089b333f24cff415a7b7e
kathrynsiegel <ksiegel@mit.edu>
2015-12-04 09:18:52 -0500

    marilyn monroe pictures

--- public/javascripts/slider.js
+++ public/javascripts/slider.js
@@ -8,4 +8,15 @@ $(document).ready(function() {
         return value + '% Van Gogh, ' + (100-value) + "% Picasso";
       }
   }).on('slide', changeImage);
+
+  var changeImage2 = function() {
+    $('.mp-shown').removeClass('mp-shown');
+    console.log($('#mp-slider').val());
+    $($('.mp-img')[$('#mp-slider').val()-1]).addClass('mp-shown');
+  };
+  var styleSlider = $('#mp-slider').bootstrapSlider({
+    formatter: function(value) {
+      return "alpha/beta value: 10e" + value;
+    }
+  }).on('slide', changeImage2);
 });
