9f89a5668554d29d5b0b54d2b13166dafa1d84d7
kathrynsiegel <ksiegel@mit.edu>
2015-12-06 16:56:08 -0500

    popovers for original images

--- javascripts/slider.js
+++ javascripts/slider.js
@@ -19,8 +19,17 @@ $(document).ready(function() {
       if (value % 2 == 0) {
         return "alpha/beta value: 5e" + value/2;
       } else {
-        return "alpha/beta value: 1e" + value;
+        return "alpha/beta value: 1e" + (value+1)/2;
       }
     }
   }).on('slide', changeImage2);
+  var content_input_1_img = '<img src="images/2-content.jpg" style="width:100%"/>';
+  $('#content-input-1').popover({ title: 'Picture of Stata', content: content_input_1_img, html:true});
+  var style_input1_1_img = '<img src="images/2-style1.jpg" style="width:100%"/>';
+  $('#style-input1-1').popover({ title: 'Picasso\'s \"Dora Maar\"', content: style_input1_1_img, html:true});
+  var style_input2_1_img = '<img src="images/2-style2.jpg" style="width:100%"/>';
+  $('#style-input2-1').popover({ title: 'Van Gogh\'s \"The Starry Night\"', content: style_input2_1_img, html:true});
+
+
+
 });
